a43b70ecd9f724e946e9b298060e10142a98b9be
Slawomir Pacek <slawomir_pacek@op.pl>
2026-03-20 13:27:14 +0100

    stream11

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -33,6 +33,6 @@ public class CandidateValidator implements Predicate<Candidate> {
         int start = Integer.parseInt(years[0]);
         int end = Integer.parseInt(years[1]);
 
-        return (end - start) >= 10;
+        return (end - start + 1) >= 10;
     }
 }
